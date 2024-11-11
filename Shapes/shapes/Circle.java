4d2833e41177b1ceca0dafef0cc9ee9bdac42586
Darshan Mahadik <125591947+Mahadik98@users.noreply.github.com>
2024-11-11 22:37:21 +0530

    JAVA ASISGNMENTS

---
+++ Shapes/shapes/Circle.java
@@ -0,0 +1,18 @@
+package shapes;
+
+public class Circle extends Shape{
+
+    private final double radius;
+
+    public Circle(double radius){
+        super();
+        this.radius = radius;
+
+    }
+
+    @Override
+    public double calArea(){
+        return 3.14 * radius*radius;
+    }
+    
+}
